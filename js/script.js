4bd0523112be5db33a99d9826bc40b641980c6b5
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-09 16:21:57 +0200

    Finito esercizio
    provo ad aggiungere qualcosa

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-var lista = ["pincopallo@gmail.com" , "giovanpaolo@hotmail.it" , "riccardofuffolo@umail.com"];
-var mail = prompt("Inserisci la tua mail");
-var not = false;
-for (var i= 0; i <= lista.length ; i++) {
-  if (mail== lista[i]) {
-    ok= true;
-  }
-}
-if (ok==true) {
-  alert("mail corretta");
+// var lista = ["pincopallo@gmail.com" , "giovanpaolo@hotmail.it" , "riccardofuffolo@umail.com"];
+// var mail = prompt("Inserisci la tua mail");
+// var ok = false;
+// for (var i= 0; i <= lista.length ; i++) {
+//   if (mail== lista[i]) {
+//     ok= true;
+//   }
+// }
+// if (ok==true) {
+//   alert("mail corretta");
+// } else {
+//   alert("mail sbagliata");
+// }
+// In questo caso ho usato una var sentinella (ok) in modo che la condizione
+// vada ad applicarsi solo al valore inserito. Infatti, in questo modo, il
+// risultato che riceverò sarà limitato a quello della variabile sentinella.
+
+var dadoutente = Math.floor(Math.random()*6)+1;
+var dadocpu = Math.floor(Math.random()*6)+1;
+console.log(dadoutente);
+console.log(dadocpu);
+var result;
+if (dadoutente > dadocpu) {
+     result = "Hai Vinto!";
+} else if (dadoutente = dadocpu) {
+    result = "Pari!";
 } else {
-  alert("mail sbagliata");
+    result = "Hai perso!"
 }
+console.log(result);
+document.getElementById('dadoutente').innerHTML = dadoutente + "  " + dadocpu + result;
